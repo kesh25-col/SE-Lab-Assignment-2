826dcbc2a0af0e71b3fa21e9691f7f4d06b15a1a
kesh25-col <kesh.pv@gmail.com>
2025-02-04 09:43:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! This is my first GitHub project!" << std::endl;
+    std::cout << "Hello, GitHub!" << std::endl;
     return 0;
 }
